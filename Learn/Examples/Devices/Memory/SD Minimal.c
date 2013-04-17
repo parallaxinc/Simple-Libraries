4f8cf0f31ce1324d709f8b40364c74345c67ee4a
Andy Lindsay <alindsay@parallax.com>
2013-04-16 21:39:55 -0700

    Candidate 02 for SimpleIDE v0.9.21 workspace folder.

---
+++ Learn/Examples/Devices/Memory/SD Minimal.c
@@ -0,0 +1,29 @@
+/*
+  SD Minimal.side
+  Minimal SD file read and write.
+*/
+
+#include "simpletools.h"                      // Include simpletools header    .
+
+int DO = 22, CLK = 23, DI = 24, CS = 25;      // SD card pins on Propeller BOE
+
+int main(void)                                // Main function
+{
+  pause(1000);                                // Delay for terminal
+
+  sd_mount(DO, CLK, DI, CS);                  // Mount SD card
+
+  FILE* fp = fopen("test.txt", "w");          // Open a file for writing
+  fprintf(fp, "Testing 123...\n");            // Add contents to the file
+  fclose(fp);                                 // Close the file
+  
+  char s[15];                                 // Buffer for characters.
+  fp = fopen("test.txt", "r");                // Reopen file for reading.
+  fread(s, 1, 15, fp);                        // Read 21 characters
+  fclose(fp);                                 // Close the file.
+
+  printf("First 21 chars in test.txt:\n");    // Display heading
+  printf("%s", s);                            // Display characters
+  printf("\n");                               // With a newline at the end.
+}    
+
