252cf284bace6e354038c7ae85f90626904a6637
Andy Lindsay <alindsay@parallax.com>
2013-07-12 18:15:08 -0700

    Fix bug in pre-compass library compass example.

--- Learn/Examples/Devices/Sensor/Compass 3-Axis HMC5883L/Test Compass HMC5883L.c
+++ Learn/Examples/Devices/Sensor/Compass 3-Axis HMC5883L/Test Compass HMC5883L.c
@@ -14,7 +14,7 @@ int main()                                    // Main function
 {
   int x, y, z;                                // Declare x, y, & z axis variables
   
-  i2c bus = i2c_newbus(3, 2);                 // New I2C bus SCL=P3, SDA=P2
+  i2c *bus = i2c_newbus(3, 2, 0);             // New I2C bus SCL=P3, SDA=P2
   compass_init(bus);                          // Initialize compass on bus.
 
   while(1)                                    // Repeat indefinitely
@@ -22,6 +22,15 @@ int main()                                    // Main function
     compass_read(bus, &x, &y, &z);            // Compass vals -> variables
     print("%cx=%d, y=%d, z=%d%c\n",           // Display compass variables
            HOME, x, y, z, CLREOL);
+
+    float fy = (float) y;                     // Ints to floats
+    float fx = (float) x;
+    float heading = atan2(fy, fx) * 180.0/PI; // Calculate heading with floats
+    if(heading < 0.0)                         // Convert -180...+180 to 0...360
+      heading = (360.0 + heading);
+    print("heading = %.2f%c\n",               // Display heading
+                        heading, CLREOL);       
+
     pause(500);                               // Wait 1/2 second
   }
 }
