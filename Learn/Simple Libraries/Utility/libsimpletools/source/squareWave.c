c0b53618b79c74c07ee149c6fe66f620acd7d1fa
Andy Lindsay <alindsay@parallax.com>
2014-08-26 20:57:17 -0700

    Adjust stacks in simpletools library.

--- Learn/Simple Libraries/Utility/libsimpletools/source/squareWave.c
+++ Learn/Simple Libraries/Utility/libsimpletools/source/squareWave.c
@@ -24,7 +24,7 @@
 #define CTR_PLL (0b10 << 26);
 #endif
 
-static unsigned int stack[(160 + (50*4)) / 4];       
+static unsigned int stack[44 + 20];       
 
 static volatile unsigned int cog = 0;
 static volatile int ctra, ctrb, frqa, frqb;
