4aa599377eef632b5e50c1e2e1cd9cf24f38aa7e
Matthew Matz <mmatz@parallax.com>
2017-03-07 00:32:01 -0800

    Fixed boxFilled function

--- Learn/Simple Libraries/Social/libbadgetools/oled_box.c
+++ Learn/Simple Libraries/Social/libbadgetools/oled_box.c
@@ -17,7 +17,7 @@ void box(int32_t x0, int32_t y0, int32_t x1, int32_t y1, int32_t c)
 void boxFilled(int32_t x0, int32_t y0, int32_t x1, int32_t y1, int32_t c)
 {
   // Draw a box formed by the coordinates of a diagonal line
-  if(y0 < y1) {
+  if(y0 > y1) {
     int temp = y0;
     y0 = y1;
     y1 = temp;
