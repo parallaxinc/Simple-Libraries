8df013a92b7acee0f21500af7f43d93dbaf6394e
Matthew Matz <mmatz@parallax.com>
2017-04-11 11:24:46 -0700

    abdrive updates

--- Learn/Simple Libraries/Robotics/ActivityBot/libabdrive/monitor.c
+++ Learn/Simple Libraries/Robotics/ActivityBot/libabdrive/monitor.c
@@ -1,233 +1,195 @@
-#include "abdrive.h"                   // Include servo lib funct defs
-#include "simpletools.h"
-#include "simpletext.h"
-#include "fdserial.h"
-#include <string.h>
+#include "abdrive.h"
 
-//#define _monitor_
+//void interpolation_table_setup();
 
+//int abd_intTabSetup;
 
 #ifdef _monitor_
-void drive_com(int arrayLcnt, int arrayRcnt, 
-               int centerL, int centerR, 
-               int* pwAddrL, int* pwAddrR, 
-               int* spdAddrL, int* spdAddrR);
-void drive_set(int left, int right);
-void encoders(void *par);
-void interpolate(int* ltmp, int* rtmp);
-void interpolation_table_setup();
-void servos_diffDrive(void);
-void drive_record(int startStop);
-void drive_displayControlSystem(int start, int end);
-void set_drive_speed(int left, int right);
-void interpolate2(int *ltmp, int *rtmp);
-void drive_rampStep2(int left, int right);
+  volatile char abd_str[128];
+#endif
 
-volatile char abd_str[128];
+//#define test_t_interval
+#ifdef test_t_interval
+  volatile int rec_t[8000 / 4];
+#endif
 
-// drive_trimset
-volatile int abd_trimFL, abd_trimFR, abd_trimBL, abd_trimBR, abd_trimticksF, abd_trimticksB;
-volatile int abd_trimticksF;
-volatile int abd_trimticksB;
-int abd_eeAddr;
+//volatile int abd_sampleCount;
 
-volatile int abd_aL;                                        // Acceleration in ticks/(s^2)
-volatile int abd_aR;
-
-volatile int abd_speedLold;
-volatile int abd_speedRold;
-volatile int abd_stopCtrL;
-volatile int abd_stopCtrR;
-volatile int abd_stopPulseRepsL;
-volatile int abd_stopPulseRepsR;
-
-// drive_goto
-volatile int abd_ticksL;                                // Measured distance left/right
-volatile int abd_ticksR;
-volatile int abd_speedLT;                                   // Target speed left/right
-volatile int abd_speedRT;                                   
-volatile int abd_speedL;                                    // Current requested speed
-volatile int abd_speedR;                                   
-
-volatile int abd_ticksLi;
-volatile int abd_ticksLf;
-
-volatile int abd_ticksRi;
-volatile int abd_ticksRf;
-
-volatile int abd_gotoFlagL;
-volatile int abd_gotoFlagR;
-//volatile int abd_ticksGuardL;
-//volatile int abd_ticksGuardR;
-
-
-volatile int abd_rampStep;
-volatile int abd_rampStepL;
-volatile int abd_rampStepR;
-volatile int abd_speedLimit;
-volatile int abd_speedLimitL;
-volatile int abd_speedLimitR;
-volatile int abd_ticksGuardL;
-volatile int abd_ticksGuardR;
-
-
-volatile int abd_zeroDelay;
-
-volatile int abd_dlc;                                      // distance left calculated
-volatile int abd_drc;                                      // distance right calculated
-volatile int abd_dlca;                                 // distance left calculated (accumulated)
-volatile int abd_drca;                                 // distance right calculated (accumulated)
-volatile int abd_dsr;                                // Distance sampling rate
-
-
-volatile int abd_edL;                                      // error distance left
-volatile int abd_edR;                                      // error distance right
-volatile int abd_pL;                                       // proportional left
-volatile int abd_pR;                                       // proportional right
-volatile int abd_iL;                                       // integral left
-volatile int abd_iR;                                       // integral right
-volatile int abd_eaL;                                  // Accumulated errors L/R
-volatile int abd_eaR;
-volatile unsigned int _servoPulseReps;                     // Servo pulse counter
-
-// servoPins
-volatile int abd_sPinL, abd_sPinR;   // Global variables
-volatile int abd_ePinL, abd_ePinR;
-volatile int abd_us;
-int abd_intTabSetup;
-
-// debug
 volatile int abd_record;               // Record values to an array
 
-// display
-volatile int abd_elCntL;
-volatile int abd_elCntR;
-volatile int abd_cntrLidx;
-volatile int abd_cntrRidx;
-int abd_spdrL[120];
-int abd_spdmL[120];
-int abd_spdrR[120];
-int abd_spdmR[120];
 
-static volatile int cntrLval;
-static volatile int cntrRval;
+void drive_com(int arrayLcnt, int arrayRcnt, 
+               int centerL, int centerR, 
+               short *pwAddrL, short *pwAddrR, 
+               short *spdAddrL, short *spdAddrR);
+//void drive_set(int left, int right);
+void abd_encoders(void *par);
+//void interpolate(int* ltmp, int* rtmp);
+void interpolation_table_setup();
+//void servos_diffDrive(void);
+//void drive_record(int startStop);
+//void drive_displayControlSystem(int start, int end);
+void set_drive_speed(int left, int right);
+void interpolate2(int *ltmp, int *rtmp);
+//void drive_rampStep2(int left, int right);
+void abd_sample(void);
 
-static int cog;
+
+// Servo pulse counter
+volatile unsigned int _servoPulseReps;                     
+volatile int abd_dsr;                                // Distance sampling rate
+volatile int abd_zeroDelay;
+volatile int abd_us;
+volatile int abd_intTabSetup;
+
 //static int servoCog2 = 0;
-static unsigned int stack[44 + 252];
+//static unsigned int stack[44 + 352];
+//static int cog;
+//static unsigned int stack[44 + 252];
 //static unsigned int servoStack[(160 + (150 * 4)) / 4];
 
+short abd_spdrL[120];
+short abd_spdmL[120];
+short abd_spdrR[120];
+short abd_spdmR[120];
 
 //static int a = 0;
-static int r;
+volatile int r;
 
-//static int intTabSetup = 0;
-//static int eeStarted = 0;
+int abd_eeAddr;
 
 static volatile int trimctr;
 static volatile int dca, trimticks;
 
-static volatile int leftPrev;
-static volatile int  rightPrev;
-
 static volatile int kp[6];
-
-static volatile int tcL;
-static volatile int tcR;
-static volatile int tiL;
-
 
 static volatile int ridx;
 
-
-static volatile int tiR;
-
-static volatile int stateL;
-static volatile int stateR;
-
-static volatile int* pwL;
-static volatile int* pwR;
-static volatile int* spdL;
-static volatile int* spdR;
-
-//static volatile int etpsR;    // encoder ticks per second requested right
-//static volatile int etpsL;    // encoder ticks per second requested right
+volatile short *pwL;
+volatile short *pwR;
+volatile short *spdL;
+volatile short *spdR;
 
 static volatile int pcount;
 static volatile unsigned int _sprOld;
-static volatile unsigned int _sprNext;
 
+//static volatile int phsL;
+//static volatile int phsR;
+//static volatile int phs[L];
+//static volatile int phs[R];
+static volatile int phs[2];
 
-static volatile int ssiL; // servo speeed interpolated left
-static volatile int ssiR; // servo speeed interpolated right
+static volatile int phsr[2];
 
-static volatile int driveL;
-static volatile int driveR;
-
-static volatile int phsL;
-static volatile int phsR;
-static volatile int phsLr;
-static volatile int phsRr;
-
-static int trimFunction;
-static int encoderFeedback;
-
-static volatile int speedLprev;
-static volatile int speedRprev;
-
-volatile int nudgeIncL;
-volatile int nudgeIncR;
-
-volatile int abd_nudgeCtrL;
-volatile int abd_nudgeCtrR;
-volatile int abd_nudgeIncL;
-volatile int abd_nudgeIncR;
-volatile int abd_distErrorL;
-volatile int abd_distErrorR;
-
-volatile int signL;
-volatile int signR;
-
-volatile int abd_distL;
-volatile int abd_distR;
-
-volatile int abd_ditherAL;
-volatile int abd_ditherAR;
-volatile int abd_ditherALa;
-volatile int abd_ditherARa;
-volatile int abd_ditherALd;
-volatile int abd_ditherARd;
-volatile int abd_ditherALp;
-volatile int abd_ditherARp;
-
-volatile int abd_ditherVL;
-volatile int abd_ditherVR;
-volatile int abd_ditherVLa;
-volatile int abd_ditherVRa;
-volatile int abd_ditherVLd;
-volatile int abd_ditherVRd;
-volatile int abd_ditherVLp;
-volatile int abd_ditherVRp;
+//static int trimFunction;
+volatile int encoderFeedback;
 
 volatile int abd_blockGoto;
 
-volatile int abd_speedLi;
-volatile int abd_speedRi;
-volatile int abd_speedLd;
-volatile int abd_speedRd;
-volatile int abd_dvFlagL;
-volatile int abd_dvFlagR;
-volatile int abd_dvFlagL;
-volatile int abd_dvFlagR;
-volatile int abd_dvFlagL;
-volatile int abd_dvFlagR;
+// drive_trimset
+volatile int abd_trimF[2];
+volatile int abd_trimB[2];
 
-static int reps;
+volatile int abd_trimticksF;
+volatile int abd_trimticksB;
+
+volatile int abd_speedOld[2];
+volatile int abd_stopCtr[2];
+
+volatile int abd_stopPulseReps[2];
+// Measured distance left/right
+volatile int abd_ticks[2];                                
+// Target speed left/right
+volatile int abd_speedT[2];                                   
+// Current requested speed
+volatile int abd_speed[2];   
+
+                                 
+volatile int abd_ticksi[2];
+
+
+volatile int abd_ticksf[2];
+
+
+volatile int abd_gotoFlag[2];
+
+volatile int abd_rampStep[3];
+volatile int abd_speedLimit[4];
+
+volatile int abd_ticksGuard[2];
+
+// distance calculated
+volatile int abd_dc[2];                                      
+
+// distance calculated (accumulated)
+volatile int abd_dca[2];                                 
+
+// error distance 
+volatile int abd_ed[2];                                      
+
+// proportional
+volatile int abd_p[2];
+
+// integral
+volatile int abd_i[2];
+
+// Accumulated errors L/R
+volatile int abd_ea[2];                                  
+
+
+// servoPins
+volatile int abd_sPin[2];
+
+// Encoder Pins 
+volatile int abd_ePin[2];
+
+// display
+volatile int abd_elCnt[2];    // ?????? Instance count different
+volatile int abd_cntrIdx[2];
+
+
+// Center values
+static volatile int cntrVal[2];
+static volatile int ti[2];
+static volatile int state[2];
+// servo speeed interpolated
+static volatile int ssi[2];
+static volatile int drive[2];
+
+static volatile int speedPrev[2];
+volatile int abd_nudgeCtr[2];
+volatile int abd_nudgeInc[2];   // ??? remove ???
+volatile int abd_distError[2];
+
+volatile int sign[2];
+volatile int abd_dist[2];
+
+volatile int abd_ditherA[2];
+volatile int abd_ditherAa[2];
+volatile int abd_ditherAd[2];
+volatile int abd_ditherAp[2];
+
+volatile int abd_ditherV[2];
+volatile int abd_ditherVa[2];
+volatile int abd_ditherVd[2];
+volatile int abd_ditherVp[2];
+
+volatile int abd_speedi[2];
+volatile int abd_speedd[2];
+volatile int abd_dvFlag[2];
+
+volatile int abd_sampleCount;
+
+#ifdef _monitor_
+volatile int abd_reps;
 void monitor(void *par);
+static int cog;
+static unsigned int stack[44 + 128];
+
 
 void monitor_start(int monitorReps)
 {
-  reps = monitorReps;
+  abd_reps = monitorReps;
   cog = 1 + cogstart(monitor, NULL, stack, sizeof(stack)-1);
   simpleterm_close();
   pause(10);
@@ -237,16 +199,18 @@ void monitor_stop()
 {
   cogstop(cog - 1);
 }
-
+//
 void monitor(void *par)
 {
+  low(26);
+  low(27);
   pause(100);
   simpleterm_open();
   int n = _servoPulseReps;
-  while(n < reps)
+  while(n < abd_reps)
   {
     while(1)
-    {
+    { 
       if(n != _servoPulseReps)
       {
         n = _servoPulseReps;
@@ -264,10 +228,64 @@ void monitor(void *par)
         "dRf%d dRc%d dR%d sRT%d sR%d tgR%d gtfR%d scR%d\r"\
         "rsl%d dald%d | rsr%d dalr%d\r",
         _servoPulseReps, 
-        abd_ticksLf, abd_dlc, abd_ticksL, abd_speedLT, abd_speedL, abd_ticksGuardL, abd_gotoFlagL, abd_stopCtrL, 
-        abd_ticksRf, abd_drc, abd_ticksR, abd_speedRT, abd_speedR, abd_ticksGuardR, abd_gotoFlagR, abd_stopCtrR, 
-        abd_rampStepL, abd_ditherALd, abd_rampStepR, abd_ditherARd);
-    if(n >= reps) cogstop(cog);    
+        abd_ticksf[ABD_L], abd_dc[ABD_L], abd_ticks[ABD_L], abd_speedT[ABD_L], abd_speed[ABD_L], abd_ticksGuard[ABD_L], abd_gotoFlag[ABD_L], abd_stopCtr[ABD_L], 
+        abd_ticksf[ABD_R], abd_dc[ABD_R], abd_ticks[ABD_R], abd_speedT[ABD_R], abd_speed[ABD_R], abd_ticksGuard[ABD_R], abd_gotoFlag[ABD_R], abd_stopCtr[ABD_R], 
+        abd_rampStep[ABD_L], abd_ditherAd[ABD_L], abd_rampStep[ABD_R], abd_ditherAd[ABD_R]);
+    if(n >= abd_reps) cogstop(cog);    
   }    
 }      
+//
+
+
+
+
+
+/*
+void monitor(void *par)
+{
+  int t = CNT;
+  int dt = 0;
+  low(26);
+  low(27);
+  pause(100);
+  simpleterm_open();
+  int n = _servoPulseReps;
+  int m = 0;
+  while(_servoPulseReps < abd_reps)
+  {
+    while(1)
+    { 
+      if(m != abd_sampleCount)
+      {
+        m = abd_sampleCount;
+        dt = CNT - t;
+        dt = 80000000 / dt;
+        t = CNT;
+        break;
+      }
+    }   
+    //   
+    //if(abd_str[0])
+   // {
+   //   print("%s", abd_str);
+   //   abd_str[0] = 0;
+   // } 
+    //               
+//    print("%d %d %d %d\r",
+//        m, dt, abd_ticks[ABD_L], abd_ticks[ABD_R] );
+
+    //if((dt < 700) || (dt > 900)) putChar('!');
+    //if(dt < 750 || dt > 850) putChar(13);
+
+    print("%d", dt/100);
+    if(n >= abd_reps) cogstop(cog);    
+  }    
+}      
+*/
+
+
+
+
+
+
 #endif
--- Learn/Simple Libraries/Robotics/ActivityBot/libabdrive/rampStep.c
+++ Learn/Simple Libraries/Robotics/ActivityBot/libabdrive/rampStep.c
@@ -2,9 +2,8 @@
 
 void drive_speed(int left, int right);
 
-void drive_ramp(int left, int right)
+void drive_rampStep(int left, int right)
 {
   drive_speed(left, right);
 }
-
 
