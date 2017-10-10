e81d7bbe6cd5deef3cfbc438d01ff3387f76e236
Andy Lindsay <alindsay@parallax.com>
2017-10-10 11:36:57 -0700

    abdrive360/servo360 - try EEPROM calibration / transfer function adjusting functions #151
    Implements calibration.  ActivityBot example programs for calibration and displaying calibration settings and results ported to ActivityBot360.

---
+++ 360 Dev/ActivityBot360/ActivityBot360 Modified/ActivitiyBot Display Calibration Results.c
@@ -0,0 +1,17 @@
+/* 
+  ActivityBot Display Calibration Results.c
+
+  Display interpolation table values from calibration
+
+  http://learn.parallax.com/activitybot/calibrate-your-activitybot
+*/
+
+#include "simpletools.h"
+#include "servo360.h"
+#include "abcalibrate360.h"
+
+int main()                   
+{
+  cal_displayResults();
+}
+
