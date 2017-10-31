b7a01379c7f3d75fcf8d399a206452de145f757d
Andy Lindsay <AndyLindsay@users.noreply.github.com>
2017-10-31 15:35:00 -0700

    ActivityBot360 - Updates for learn.parallax.com filenames and paths

---
+++ Learn/Examples/Robots/ActivityBot/Forward then Stop.c
@@ -0,0 +1,15 @@
+/* 
+  Forward Stop Face Right.c
+
+  http://learn.parallax.com/activitybot/go-certain-distances
+*/
+
+#include "simpletools.h"
+#include "abdrive.h"
+
+int main()                    
+{
+  drive_speed(64, 64);
+  pause(4000);
+  drive_speed(0, 0);
+}
