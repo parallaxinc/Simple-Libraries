c29e910fe2d5af3ca1f4c882fad4ce005d57e2d5
Andy Lindsay <alindsay@parallax.com>
2017-12-18 18:34:23 -0800

    ActivityBot 360 tests for strange speed/goto transition behavior

---
+++ 360 Dev/ActivityBot360/ActivityBot360 Modified/Forward Stop Face Right (1).c
@@ -0,0 +1,44 @@
+/* 
+  Forward Stop Face Right.c
+
+  http://learn.parallax.com/activitybot/go-certain-distances
+*/
+
+#include "simpletools.h"
+#include "abdrive360.h"
+
+int main()                    
+{
+  /*
+  for(int n = 0; n < 4; n++)
+  {
+    drive_speed(50, 50);
+    pause(200);
+    //drive_speed(0, 0);
+    //pause(200);
+    drive_goto(26, -25);
+  }    
+
+  for(int n = 0; n < 4; n++)
+  {
+    drive_speed(50, 50);
+    pause(200);
+    drive_speed(0, 0);
+    pause(200);
+    drive_goto(26, -25);
+  }  
+
+  for(int n = 0; n < 4; n++)
+  {
+    drive_goto(25, 25);
+    drive_speed(-20, 20);
+    pause(200);
+    drive_speed(0, 0);
+    pause(200);
+  }    
+  */  
+
+  drive_speed(128, 128);
+  pause(1000);
+  drive_goto(25, -25);
+}
