45f6520d83a91c83f0883b1746ba7058850a3117
Matthew Matz <mmatz@parallax.com>
2018-01-05 00:19:23 -0800

    Fix ADC function

--- Learn/Simple Libraries/Robotics/S3/libs3/libs3.c
+++ Learn/Simple Libraries/Robotics/S3/libs3/libs3.c
@@ -74,8 +74,21 @@ the Test Utility, cycle the Scribbler//s power switch.
 
 */
 
-int main()
-{
+#include "simpletools.h"
+#include "s3.h"
+
+
+
+// ------ Main Program ------
+int main() {
+  s3_setup();
+  pause(100);
+  simpleterm_reopen(31, 30, 0, 9600);
+  while (1) {
+    print("%d", s3_readADC(S3_ADC_A0));
+    print("%c", 13);
+    pause(500);
+  }
 
 }
 
