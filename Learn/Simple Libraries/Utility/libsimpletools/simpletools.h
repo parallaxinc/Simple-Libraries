1cc167c103756e5772b533c833a0f34e6050f459
AndyLindsay <alindsay@parallax.com>
2020-11-19 18:22:58 -0800

    Add pending commits
    Today:
    
    Merge Simple Libraries Updates into main
    
    Merge pending commits into main
    
    Remove MCP 3002 references from simpletools.h
    
    IMPORTANT: These commits still have to be validated before adding to Simple Library Updates or BlocklyProp.

--- Learn/Simple Libraries/Utility/libsimpletools/simpletools.h
+++ Learn/Simple Libraries/Utility/libsimpletools/simpletools.h
@@ -62,7 +62,6 @@
  * @par
  * 1.1.7 Update pause function for up to 2,147,483,647 ms.  
  * @par
- * 0.98.3 Integrated MCP3X02 support with function "spi_adc" and wrapper functions "MCP3202_getVolts" and "MCP3002_getVolts"
  * 0.98.2 Add term_cmd function for SimpleIDE Terminal cursor, screen, and audio
  * control.  
  * @par
@@ -129,7 +128,7 @@ extern "C" {
 #include <sys/sd.h>
 #include <math.h>
 #include "simplei2c.h"
-#include "spi_adc.h"
+
 
 /**
  * @brief Propeller system clock ticks in 1 millisecond (ms).
